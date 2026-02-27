add80a386eb5a6dc65543f748f639abddda044c2
Ashikanparasu <ashikanparasu@gmail.com>
2026-02-28 04:52:28 +0530

    RequirementAdded

---
+++ MiniProject/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+//import './App.css'
+
+import Button from '@mui/material/Button';
+
+function App() {
+  
+
+  return (
+    <>
+      <h1> Demo </h1>
+      <Button variant ="contained">Click Me !</Button>
+    </>
+  );
+}
+
+export default App;
